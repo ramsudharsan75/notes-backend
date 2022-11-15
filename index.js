7b06c6275a24cbbdd103a736faeb0c281e29a076
ramsudharsan75 <ramsudharsan.nitdgp@gmail.com>
2022-11-15 20:04:41 +0530

    Connected to Mongo Atlas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
 
 const app = express();
 
@@ -7,92 +9,52 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
+app.get("/api/notes", (request, response) =>
+  Note.find({}).then((notes) => response.json(notes))
+);
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+app.get("/api/notes/:id", (request, response) =>
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
+  })
+);
 
-app.put("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const updatedNote = { ...notes.find((note) => note.id === id) };
+app.put("/api/notes/:id", (request, response) =>
+  Note.findById(request.params.id).then((note) => {
+    note.important = !note.important;
+    Note.findByIdAndUpdate(request.params.id, note).then(() =>
+      response.json(note)
+    );
+  })
+);
 
-  if (!updatedNote) {
-    return response.status(400).json({
-      error: "The requested note has already been removed from the server",
-    });
-  }
-
-  updatedNote.important = !updatedNote.important;
-  notes = notes.map((note) => (note.id === id ? updatedNote : note));
-  response.status(200).send(updatedNote);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.delete("/api/notes/:id", (request, response) =>
+  Note.findByIdAndDelete(request.params.id).then(() =>
+    response.status(204).send()
+  )
+);
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
